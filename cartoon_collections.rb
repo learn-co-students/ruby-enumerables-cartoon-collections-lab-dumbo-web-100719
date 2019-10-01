7433a4ce8c336629169a977313cddd5c2042a050
Raq Robinson <robin.raq@gmail.com>
2019-10-01 22:26:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names) 
   # Your code here
+  names.each_with_index {|dwarf, index|
+    puts "/#{index+1}. #{dwarf}/"
+  }
+  
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
   # Your code here
+  planeteer_calls.map! {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (short_words)
   # Your code here
+  short_words.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  (food).find  { |i|  cheese_types.any?(i) } 
+  
 end
+
