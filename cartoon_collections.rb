a5511be120d0a7cc5d06687aedd7c1b89661e697
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-10-01 23:35:15 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) # code an argument here
+  array.each do |dwarf| 
+    puts "#{array.index(dwarf) + 1} #{dwarf}"    
+  end 
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array) 
+  captain_call = array.map do |ele| 
+    ele.capitalize + "!"  
+  end 
+  captain_call
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array) 
+  array.each { |ele| return true if ele.length > 4 }
+  
+  false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.each do |ele| 
+    if cheese_types.include?(ele)
+      return ele 
+    end 
+  end 
+  nil 
 end
